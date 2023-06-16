483302b52e4b6907aac1b674249ba4796edff4e4
Angel arcos <angel_arcos@outlook.es>
2023-06-16 15:00:12 +0100

    del apiconfig

--- laravel-SafeBet/config/apiBroker.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [  
-    'key' => "BEARER eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI2MTk5ODM5Y2Q1YTU3ZTVjZjM3YjNhZmEiLCJpYXQiOjE2Mzc0NTA5NDEsImV4cCI6MTczMjA1ODk0MX0.njT53vBXYHuTIEwJ4QeVgzW8pdOleAZsRgsuHUThWfA",
-    'content' => "application/json",
-];
-
